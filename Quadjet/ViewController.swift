7dd3b2dea13a87abe3f08b65ce5aeed0c9284deb
Andrew Chersky <andrew.chersky@gmail.com>
2023-03-21 19:39:31 +0100

    Initial Commit

---
+++ Quadjet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quadjet
+//
+//  Created by Andrew Chersky on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
